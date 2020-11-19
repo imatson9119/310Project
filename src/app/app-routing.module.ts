770af47c60d16c9d625a0b333a027664c6f2f11c
sneal <sneal@tamu.edu>
2020-11-18 21:06:44 -0600

    menu component added p1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 // const routes: Routes = [
 //   { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -17,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
    { path: "", redirectTo: "/home", pathMatch: "full" },
    { path: "home", component: HomeComponent },
+   { path: "menu", component: MenuComponent },
 ];
 
 @NgModule({
