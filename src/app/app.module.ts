2eade22710f0d101410f2e9cd7f490ab35d8138a
Ian Matson <imatson9119@gmail.com>
2020-11-30 10:23:16 -0600

    Began recent expenses page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,9 @@ import { BudgetingComponent } from './budgeting/budgeting.component';
 import { UserProfileComponent } from './shared/user-profile/user-profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 const config = {
   apiKey: "AIzaSyAEUJ9BQIKXEK72X6RwYGE-X3tBlYWPDTc",
@@ -48,7 +51,11 @@ const config = {
     AngularFirestoreModule,
     AngularFireAuthModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+    
   ],
   providers: [LoggedInRouteGuard],
   bootstrap: [AppComponent]
