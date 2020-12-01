f501f22c016802cad848bec3fb90d89fcf439236
Ian Matson <imatson9119@gmail.com>
2020-12-01 13:54:37 -0600

    Group members now affect checks

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument, DocumentSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { User } from '../shared/models/user.model';
@@ -6,9 +6,12 @@ import { User } from '../shared/models/user.model';
 @Injectable({
   providedIn: 'root'
 })
-export class FirestoreService {
+export class FirestoreService{
 
-  constructor(public firestore: AngularFirestore) { }
+  expenseTypes: string[] = []
+
+  constructor(public firestore: AngularFirestore) { this.getExpenseTypes()}
+
 
   async getUsers(uids: string[]): Promise<User[]>{
     let promises = []
@@ -22,5 +25,13 @@ export class FirestoreService {
   async getUser(uid: string): Promise<User>{
     const userRef: AngularFirestoreDocument<User> = this.firestore.doc(`users/${uid}`);
     return (await userRef.get().toPromise()).data()
+  }
+
+  getExpenseTypes(){
+    this.firestore.collection("ExpenseTypes").get().subscribe(data => {
+      data.docs.forEach(doc => {
+        this.expenseTypes.push(doc.id);
+      })
+    })
   }
 }
